5ec7dd1c425f2598abfcef706b2aebed01054bc4
neilpang <github@byneil.com>
2017-04-09 19:51:27 +0800

    fix

--- 5/run.sh
+++ 5/run.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env sh
 
 [ "$SSL_HOST_NAMES" ] \
-&& cp /etc/apache2/sites-available/default-ssl.conf /etc/apache2/sites-enabled \
+&& service apache2 start \
 && /root/.acme.sh/acme.sh --issue \
 -d "$SSL_HOST_NAMES" \
 -w /var/www/html \
 --key-file /etc/ssl/private/ssl-cert-snakeoil.key \
 --fullchain-file /etc/ssl/certs/ssl-cert-snakeoil.pem \
---reloadcmd "service apache2 force-reload"
+--reloadcmd "service apache2 force-reload" \
+&& cp /etc/apache2/sites-available/default-ssl.conf /etc/apache2/sites-enabled \
+&& service apache2 stop
 
 apache2-foreground
