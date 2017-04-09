fc7d834bb47d5c702bfbda4e0e9c0646193ec872
neilpang <github@byneil.com>
2017-04-09 18:53:04 +0800

    fix

--- 7/run.sh
+++ 7/run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 [ "$SSL_HOST_NAMES" ] \
+&& cp /etc/apache2/sites-available/default-ssl.conf /etc/apache2/sites-enabled \
 && /root/.acme.sh/acme.sh --issue 
 -d "$SSL_HOST_NAMES" \
 -w /var/www/html \
