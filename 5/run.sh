b2f22ebb4d8a4552e690e8cc6523f8258eefde77
neilpang <github@byneil.com>
2017-04-09 19:43:48 +0800

    fix

--- 5/run.sh
+++ 5/run.sh
@@ -2,7 +2,7 @@
 
 [ "$SSL_HOST_NAMES" ] \
 && cp /etc/apache2/sites-available/default-ssl.conf /etc/apache2/sites-enabled \
-&& /root/.acme.sh/acme.sh --issue 
+&& /root/.acme.sh/acme.sh --issue \
 -d "$SSL_HOST_NAMES" \
 -w /var/www/html \
 --key-file /etc/ssl/private/ssl-cert-snakeoil.key \
