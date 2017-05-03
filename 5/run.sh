bee15e1deb63ac39fe10968e345c9ed8217bcf2c
neil <github@byneil.com>
2017-05-03 10:10:01 +0800

    fix ssl site

--- 5/run.sh
+++ 5/run.sh
@@ -9,9 +9,12 @@
 -w /var/www/html \
 --key-file /etc/ssl/private/ssl-cert-snakeoil.key \
 --fullchain-file /etc/ssl/certs/ssl-cert-snakeoil.pem \
---reloadcmd "service apache2 force-reload" \
-&& cp /etc/apache2/sites-available/default-ssl.conf /etc/apache2/sites-enabled \
-&& service apache2 stop
+--reloadcmd "service apache2 force-reload"
 
+
+[ -f "/etc/ssl/private/ssl-cert-snakeoil.key" ] \
+  && cp /etc/apache2/sites-available/default-ssl.conf /etc/apache2/sites-enabled
+
+service apache2 stop
 chown -R www-data:www-data /var/www/html
 apache2-foreground
